7c56ee1f1b9ce75426bbb306a5c7678e928edb60
Andrew <at100991@student.sgh.waw.pl>
2020-11-15 11:02:57 +0200

    Change styles of input,add text after button

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let btn = document.querySelector('.btn');
 let input = document.querySelector('input');
+let content = document.querySelector('.after-text');
 
 
 btn.addEventListener('click',(e) => {
+
    let userAnswerNumber = parseInt(input.value);
    if(Number.isInteger(userAnswerNumber)){
-   
+      let textAfterGenerate = document.createTextNode(`You generated ${input.value} cats if you want new cat image please refresh the page`);
+      content.appendChild(textAfterGenerate);
 
 for(let i = 0; i < input.value; i++) {
    let imgContainer = document.createElement('div');
