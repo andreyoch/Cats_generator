8dcd3e77d46c94aada8035321642280bf9f9476d
Andrew <at100991@student.sgh.waw.pl>
2020-11-15 20:14:34 +0200

    Code refactor and format

--- main.js
+++ main.js
@@ -1,35 +1,32 @@
-let btn = document.querySelector('.btn');
-let input = document.querySelector('input');
-let content = document.querySelector('.after-text');
+let btn = document.querySelector(".btn");
+let input = document.querySelector("input");
+let container = document.querySelector(".flex-row");
+let content = document.querySelector(".after-text");
 
+let generateImg = () => {
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("img-container");
+  let img = document.createElement("img");
+  imgContainer.appendChild(img);
+  img.src =
+    "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+  container.appendChild(imgContainer);
+};
 
-btn.addEventListener('click',(e) => {
+btn.addEventListener("click", (e) => {
+  let userAnswerNumber = parseInt(input.value);
 
-   let userAnswerNumber = parseInt(input.value);
-   if(Number.isInteger(userAnswerNumber)){
-      let textAfterGenerate = document.createTextNode(`You generated ${input.value} cats if you want new cat image please refresh the page`);
-      content.appendChild(textAfterGenerate);
+  if (Number.isInteger(userAnswerNumber)) {
+    content.appendChild(
+      document.createTextNode(
+        `You generated ${input.value} cats if you want new cat image please refresh the page`
+      )
+    );
 
-for(let i = 0; i < input.value; i++) {
-   let imgContainer = document.createElement('div');
-   imgContainer.classList.add('img-container');
-   imgContainer.classList.add('img');
-   let img = document.createElement('img');
-   imgContainer.appendChild(img);
-   img.src = 'http://thecatapi.com/api/images/get?format=src&type=gif&size=small'
-   let container = document.querySelector('.flex-row');
-   container.appendChild(imgContainer);
-}
-
-
-
-}
-   else {
-      alert('Please provide a number');
-   }
-
+    for (let i = 0; i < input.value; i++) {
+      generateImg();
+    }
+  } else {
+    alert("Please provide a number");
+  }
 });
-
-
-
-
